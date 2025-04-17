a3c362348fa551bdc6b78b25e62ccf4c02ad7122
NightingaleWK <djsywxsj@outlook.com>
2025-04-17 17:10:01 +0800

    安装 Filament Spatie Settings Plugin 扩展，并配置一个预设的展位内容

---
+++ app/Filament/Pages/ManageSystem.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Settings\SystemSetting;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Pages\SettingsPage;
+
+class ManageSystem extends SettingsPage
+{
+    protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
+
+    protected static string $settings = SystemSetting::class;
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('site_name')->required(),
+                Toggle::make('site_active')->required(),
+            ]);
+    }
+}
