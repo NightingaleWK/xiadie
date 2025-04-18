c01fafa7fbf690ac8377c2d6dc1f689af9bf51e2
NightingaleWK <djsywxsj@outlook.com>
2025-04-18 16:24:40 +0800

    调整setting细节

--- database/settings/2025_04_17_085422_create_general_settings.php
+++ database/settings/2025_04_17_085422_create_general_settings.php
@@ -6,7 +6,7 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('general.site_name', '招远中电运维工作平台');
+        $this->migrator->add('general.site_name', '运维工作平台');
         $this->migrator->add('general.site_copyright', '招远中电智慧产业发展有限公司');
         $this->migrator->add('general.site_telephone', '0535-8218808');
         $this->migrator->add('general.site_address', '山东省烟台市招远市温泉路128号金融大厦10楼1002室');
