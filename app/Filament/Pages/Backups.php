6ee38168fe7ebb69767248de22a926590a3c04fd
NightingaleWK <djsywxsj@outlook.com>
2025-04-18 14:53:04 +0800

    修改backup在role中的显示效果

--- app/Filament/Pages/Backups.php
+++ app/Filament/Pages/Backups.php
@@ -7,4 +7,6 @@ use ShuvroRoy\FilamentSpatieLaravelBackup\Pages\Backups as BaseBackups;
 class Backups extends BaseBackups
 {
     protected static ?string $navigationIcon = 'heroicon-o-circle-stack';
+    protected static ?string $navigationGroup = '系统设置';
+    protected static ?string $title = '数据备份';
 }
